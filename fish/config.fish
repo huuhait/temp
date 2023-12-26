c76cb02ec33ca1d39ccf265fc841536050a3e75a
huuhait <huuhadz2k@gmail.com>
2023-12-26 22:13:28 +0700

    up

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+
+end
+
+alias clear='clear && neofetch'
+export GITHUB_TOKEN=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
+export GOPRIVATE=github.com/zsmartex,github.com/safetrade-exchange
+
+starship init fish | source
+neofetch
+
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
